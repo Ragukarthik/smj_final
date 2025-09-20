f09e985d6fad8bc8fc39def9481482f82ea61635
Ragukarthik <ragukarthik1997@gmail.com>
2025-09-20 12:39:09 +0530

    Fix logo import paths

--- app/login.tsx
+++ app/login.tsx
@@ -92,7 +92,7 @@ export default function LoginScreen() {
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         <View style={styles.logoContainer}>
           <Image 
-            source={require('../assets/logo.png')} 
+            source={require('../assets/images/logo.png')} 
             style={styles.logo}
             resizeMode="contain"
           />
